d4e0753368a0cd925f46af2d1dc2e21f379ecff8
aadiityaak <aadiityaak@gmail.com>
2020-11-19 07:29:34 +0700

    Init

---
+++ justgaddons.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Justg Addon
+ * Plugin URI: http://www.velocitydeveloper.com
+ * Description: Shortcode & modules for justg theme
+ * Version: 2.0
+ * Author: Velocitydeveloper
+ * Author URI: http://www.velocitydeveloper.com
+ */
+define( 'FL_MODULE_EXAMPLES_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FL_MODULE_EXAMPLES_URL', plugins_url( '/', __FILE__ ) );
+
+require_once FL_MODULE_EXAMPLES_DIR . 'classes/class-fl-custom-modules-example-loader.php';
